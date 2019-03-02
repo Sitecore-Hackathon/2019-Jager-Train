e2d9d51d14ea0ebb56c645e60e3cae144093426f
Anton Lyhin <a.lyhin@reply.com>
2019-03-02 20:41:44 +0300

    Experience Analytics FE extension (or js added from ChromeExtension);

---
+++ src/Project/Demo/code/ChromeExtensionJs/LogItemIds.js
@@ -0,0 +1,5 @@
+﻿jQuery('a.scContentTreeNodeNormal').mouseover((element) => {
+	var elementId = jQuery(element.currentTarget).attr('id');
+    itemId = elementId.substring(10, 10000);
+    console.log(itemId);
+});
